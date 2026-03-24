4637e7942b1f05d0106d63b144609de7ada03abd
Cuong Phan <cuongpv1991@gmail.com>
2026-03-24 12:08:12 +0700

    Fix React Router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/nhjm-study/">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/plus" element={<Plus />} />
